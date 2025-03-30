797e10de9ae9ae61752fbcdf4c31f64a3997a585
WOOJUN NAM <bjacau3@gmail.com>
2025-03-30 18:50:56 +0900

    배운거 정리
    console.log, const, function, object를 사용

---
+++ index.js
@@ -0,0 +1,30 @@
+// const player = {
+//     name: "wj",
+//     sayHello: function(otherPersonName){
+//     console.log("Hello!" + otherPersonName + "nice to meet you");
+//     },
+// };
+
+// console.log(player.name);
+// player.sayHello("lynn");
+
+
+
+const calculator = {
+    plus:function (a, b){
+        alert(a + b);
+    },
+    minus:function (a, b){
+        alert(a - b);
+    },
+    divide:function (a, b){
+        alert(a / b);
+    },
+    powerOf:function (a, b){
+        alert(a ** b);
+    },
+
+};
+
+
+console.log(calculator.plus(2, 3));
