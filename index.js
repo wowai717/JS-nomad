c49f7c52e316cefe69058e887f244d59447f4dc0
WOOJUN NAM <bjacau3@gmail.com>
2025-03-31 23:00:09 +0900

    Update index.js
    parseInt, if,else 배움

--- index.js
+++ index.js
@@ -10,24 +10,37 @@
 
 
 
-const calculator = {
-    plus:function (a, b){
-        return a + b;
-    },
-    minus:function (a, b){
-        return a - b;
-    },
-    times:function (a, b) {
-        return a * b;
-    },
-    divide:function (a, b){
-        return a / b;
-    },
-    powerOf:function (a, b){
-        return a ** b;
-    },
+// const calculator = {
+//     plus:function (a, b){
+//         return a + b;
+//     },
+//     minus:function (a, b){
+//         return a - b;
+//     },
+//     times:function (a, b) {
+//         return a * b;
+//     },
+//     divide:function (a, b){
+//         return a / b;
+//     },
+//     powerOf:function (a, b){
+//         return a ** b;
+//     },
 
-};
+// };
 
 
-const plusResult = calculator.plus(2, 3);
+// const plusResult = calculator.plus(2, 3);
+// const minusResult = calculator.minus(5, 10);
+// const timesResult = calculator.times(10, minusResult)
+// const divideResult = calculator.times(times, plusResult);
+// const powerOfResult = calculator.powerOf(divideResult, minusResult);
+
+
+const age = parseInt (prompt("How old are you?"));
+
+if(isNaN(age)){
+    console.log("Pleas write a number") 
+} else {
+    console.log("Thank you for writing your age.")
+}
