0791666f135f8e25d0f07b8f6fad27248725f47a
WOOJUN NAM <bjacau3@gmail.com>
2025-03-31 23:23:01 +0900

    or, and,
    ||,&&, else if, if, else, >,<.<=,>=

--- index.js
+++ index.js
@@ -37,10 +37,24 @@
 // const powerOfResult = calculator.powerOf(divideResult, minusResult);
 
 
-const age = parseInt (prompt("How old are you?"));
+// const age = parseInt (prompt("How old are you?"));
 
-if(isNaN(age)){
-    console.log("Pleas write a number") 
-} else {
-    console.log("Thank you for writing your age.")
-}
+// if(isNaN(age)){
+//     console.log("Pleas write a number") 
+// } else {
+//     console.log("Thank you for writing your age.")
+// }
+
+const age = parseInt(prompt("How old are you?"));
+
+if(isNaN(age) || age <0) {
+    console.log("Pleas write a real positive number");
+} else if (age < 18) {
+    console.log("you are too young.");
+} else if(age >= 18 && age <= 50){
+    console.log("you can drink");
+} else if(age >50 && age <= 80){
+    console.log("You should exercise");
+} else if(age > 80){
+    console.log("You can do whatever you want.");
+} 
