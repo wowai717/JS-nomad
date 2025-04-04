4897e17c16c59e71769531ef5c331ff0612f8b0c
WOOJUN NAM <bjacau3@gmail.com>
2025-04-04 15:55:08 +0900

    element, event
    console.dir(),
    EventListener(),
    addEventListener(),
    
    title.addEventListener("click",~~~~);
    
    window.addEventListener("    ", ~~~);

--- index.js
+++ index.js
@@ -78,8 +78,41 @@
 // const hellos = document.getElementsByClassName("hello");
 // console.log(hellos);
 
-const title = document.querySelector("div.hello:first-child h1");
-console.dir(title); 
-title.innerText = "Hello";
+const h1 = document.querySelector("div.hello:first-child h1");
 
-title.style.color ="blue"
+function handleTitleClick() {
+    h1.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("sos no wifi");
+}
+
+function handleWindowOnline() {
+    alert("ALL GOOD");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
