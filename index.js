75448f5ac4c81b5460608203d36c34c44243e018
WOOJUN NAM <bjacau3@gmail.com>
2025-04-04 16:16:34 +0900

    blue red blue red
    blue red blue red

--- index.js
+++ index.js
@@ -78,41 +78,55 @@
 // const hellos = document.getElementsByClassName("hello");
 // console.log(hellos);
 
+// const h1 = document.querySelector("div.hello:first-child h1");
+
+// function handleTitleClick() {
+//     h1.style.color = "blue";
+// }
+
+// function handleMouseEnter() {
+//     h1.innerText = "Mouse is here!";
+// }
+
+// function handleMouseLeave() {
+//     h1.innerText = "Mouse is gone!";
+// }
+
+// function handleWindowResize() {
+//     document.body.style.backgroundColor = "tomato";
+// }
+
+// function handleWindowCopy() {
+//     alert("copier!");
+// }
+
+// function handleWindowOffline() {
+//     alert("sos no wifi");
+// }
+
+// function handleWindowOnline() {
+//     alert("ALL GOOD");
+// }
+
+// h1.addEventListener("click", handleTitleClick);
+// h1.addEventListener("mouseenter", handleMouseEnter);
+// h1.addEventListener("mouseleave", handleMouseLeave);
+
+// window.addEventListener("resize", handleWindowResize);
+// window.addEventListener("copy", handleWindowCopy);
+// window.addEventListener("offline", handleWindowOffline);
+// window.addEventListener("online", handleWindowOnline);
+
 const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleTitleClick() {
-    h1.style.color = "blue";
+function handleTitleClick(){
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor ="tomato";
+    } else{
+        newColor = "blue"
+    }
+    h1.style.color = newColor;
 }
-
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("sos no wifi");
-}
-
-function handleWindowOnline() {
-    alert("ALL GOOD");
-}
-
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
