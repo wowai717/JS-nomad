7601474deb9713ec3a13fe8aafea534515e1f791
WOOJUN NAM <bjacau3@gmail.com>
2025-04-11 17:17:22 +0900

    login
    login 배움

--- index2.js
+++ index2.js
@@ -11,6 +11,11 @@ const loginButton = document.querySelector("#login-form button")
 // 더 짧게 하자면 이렇게 작성 가능, 한 element에서 찾는다
 
 function onLoginBtnClick(){
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    if(username === ""){
+        alert("Please write your name")
+    } else if(username.length > 15) {
+        alert("your name is too long.")
+    }
 }
 loginButton.addEventListener("click", onLoginBtnClick)
