5dd06f23c8216181d1d589bd3de87846b82f7b83
WOOJUN NAM <bjacau3@gmail.com>
2025-04-11 14:49:20 +0900

    toggle
    더 짧게

--- index.js
+++ index.js
@@ -121,12 +121,6 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){    
     const clickedClass = "clicked"
-    if(h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    }else {
-        h1.classList.add(clickedClass);
-    }
-
-    h1.classList.toggle("clicked"); //이게 위의 코드랑 똑같은 기능
+    h1.classList.toggle("clicked");
 }
 h1.addEventListener("click", handleTitleClick);
